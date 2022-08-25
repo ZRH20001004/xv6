9926072e100a99627c287fc8949fface95f38a3f
“ZRH20001004” <zrh20001004@163.com>
2022-08-25 14:27:28 +0800

    complete lab fs

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -309,11 +309,36 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
+    if(ip->type == T_SYMLINK && omode != O_NOFOLLOW){
+      for(int i = 0; i < 10; ++i){
+        if(i == 9){
+          iunlock(ip);
+          end_op();
+          return -1;
+        }
+        char new_path[MAXPATH];
+        if(readi(ip, 0, (uint64)new_path, 0, MAXPATH) == 0){
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        iunlockput(ip);
+        if((ip = namei(new_path)) == 0){
+          end_op();
+          return -1;
+        }
+        ilock(ip);
+        if(ip->type != T_SYMLINK)
+          break;
+      }
+    }
+
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
       return -1;
     }
+    
   }
 
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
@@ -483,4 +508,35 @@ sys_pipe(void)
     return -1;
   }
   return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH], path[MAXPATH];
+  int n;
+  struct inode *ip;
+
+  if((n = argstr(0, target, MAXPATH)) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0)
+  {
+    end_op();
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)target, 0, n) < 0)
+  {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
+  return 0;
+
 }
